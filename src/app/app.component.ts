85d3ba1cc14e140558ebbc9333c6e4759a4e729e
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2021-11-21 17:09:18 +0300

    With Ng for items be sorted.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = "My Angular App";
+  items = [
+    { Description: "Breakfast", Action: "No" },
+    { Description: "Sport", Action: "No" },
+    { Description: "Bill", Action: "No" },
+    { Description: "Cinema", Action: "No" },
+  ];
 }
