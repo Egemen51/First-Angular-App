78f436586bd9b44cf8ecb7d4c778d73a91248647
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2021-11-22 11:01:36 +0300

    All of them steps completed. Ready to use.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Model, TodoItem } from "./model";
 import { Component } from "@angular/core";
 
 @Component({
@@ -6,11 +7,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "My Angular App";
-  items = [
-    { Description: "Breakfast", Action: "No" },
-    { Description: "Sport", Action: "No" },
-    { Description: "Bill", Action: "No" },
-    { Description: "Cinema", Action: "No" },
-  ];
+  IsDisplay = false;
+  model = new Model();
+  getItems() {
+    if (this.IsDisplay) {
+      return this.model.items;
+    }
+    return this.model.items.filter((item) => !item.action);
+  }
+  addItem(val) {
+    if (val != "") {
+      this.model.items.push(new TodoItem(val, false));
+    }
+  }
 }
